2675aec2ff543885701e68266848b18d3853035c
wangxiaoyan <wxy20850606@gmail.com>
2023-06-10 15:20:23 +0800

    fix merge conflict file bug

--- proj2/gitlet/GitletRepository.java
+++ proj2/gitlet/GitletRepository.java
@@ -591,9 +591,9 @@ public class GitletRepository implements Serializable {
                     continue;
                 } else {
                     /** files in two head commits are different from splitPoint commit, conflict*/
-                    boolean x = !(spl.getMap().get(fileName).equals(cur.getMap().get(fileName)));
-                    boolean y = !(spl.getMap().get(fileName).equals(tar.getMap().get(fileName)));
-                    if ((x && y) || (!inCurrent && inTarget) || (inCurrent && !inTarget)) {
+                    boolean x = (spl.getMap().get(fileName).equals(cur.getMap().get(fileName)));
+                    boolean y = (spl.getMap().get(fileName).equals(tar.getMap().get(fileName)));
+                    if ((!x && !y) || (!inCurrent && inTarget) || (inCurrent && !inTarget)) {
                         String blobID = handelMergeConflict(fileName, cur, tar);
                         newMap.put(fileName, blobID);
                         conflictFlag = true;
